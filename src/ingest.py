c103b4d0ef4091a0b76b4cf8aa3bd70abf12c405
Lilllian Rocha <lillianrocha.negocio@gmail.com>
2025-09-04 20:08:24 -0300

    add ingest.py

--- src/ingest.py
+++ src/ingest.py
@@ -1,13 +1,50 @@
 import os
 from dotenv import load_dotenv
+from langchain_community.document_loaders import PyPDFLoader
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+from langchain_core.documents import Document
+from langchain_google_genai import GoogleGenerativeAIEmbeddings
+from langchain_postgres import PGVector
 
 load_dotenv()
 
 PDF_PATH = os.getenv("PDF_PATH")
 
 def ingest_pdf():
-    pass
+    docs = PyPDFLoader(os.getenv("PDF_PATH")).load()
+    
+    splits = RecursiveCharacterTextSplitter(
+        chunk_size=1000,
+        chunk_overlap=150,
+        add_start_index=False
+    ).split_documents(docs)
+    if not splits:
+        raise SystemExit(0)
 
+    enriched = []
+    for d in splits:
+        meta = {k: v for k, v in d.metadata.items() if v not in ("", None)}
+        new_doc = Document(
+            page_content=d.page_content,
+            metadata=meta
+        )
+        enriched.append(new_doc)
+
+    ids = [f"doc-{i}" for i in range(len(enriched))]
+
+    embeddings = GoogleGenerativeAIEmbeddings(
+        model=os.getenv("GOOGLE_EMBEDDING_MODEL"))    
+    
+    store = PGVector(
+        embeddings=embeddings,
+        collection_name=os.getenv("PG_VECTOR_COLLECTION_NAME"),
+        connection=os.getenv("DATABASE_URL"),
+        use_jsonb=True,
+    )
+
+    store.add_documents(documents=enriched, ids=ids)
 
 if __name__ == "__main__":
     ingest_pdf()
+
+    
